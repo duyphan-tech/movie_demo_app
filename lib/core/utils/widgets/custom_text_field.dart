cbbf94f5c2738767cac21048d59fe47e6b2f6fc4
duy-phan <duy.phan@executionlab.asia>
2026-02-04 13:32:11 +0700

    Refactor auth feature to use `flutter_hooks` and `hooks_riverpod`
    - **Dependencies**:
      - Add `flutter_hooks` and `hooks_riverpod` to `pubspec.yaml` for hook-based state management.
    - **Features/Auth**:
      - Refactor `LoginScreen` from a `ConsumerStatefulWidget` to a `HookConsumerWidget`.
      - Replace manual `TextEditingController` management with `useTextEditingController` and `useMemoized` for the form key.
      - Manage password visibility state using the `useState` hook.
      - Implement conditional debug-mode initial values for email and password fields.
      - Simplify UI logic by removing the "Register" button and redundant "OR" divider.
    - **Core/Widgets**:
      - Implement `CustomTextField` as a reusable component to standardize text input styling and behavior across the app.

---
+++ lib/core/utils/widgets/custom_text_field.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final String? hintText;
+  final IconData? prefixIcon;
+  final Widget? suffixIcon;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final String? Function(String?)? validator;
+  final bool enabled;
+  final Function(String)? onFieldSubmitted;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    this.hintText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.keyboardType,
+    this.textInputAction,
+    this.validator,
+    this.enabled = true,
+    this.onFieldSubmitted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      obscureText: obscureText,
+      enabled: enabled,
+      validator: validator,
+      onFieldSubmitted: onFieldSubmitted,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hintText,
+        border: const OutlineInputBorder(),
+        contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+        prefixIcon: prefixIcon != null ? Icon(prefixIcon) : null,
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,71 +1,62 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../../../core/utils/widgets/custom_text_field.dart';
 import '../providers/auth_controller.dart';
 import '../providers/auth_provider.dart';
 
-class LoginScreen extends ConsumerStatefulWidget {
+class LoginScreen extends HookConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  ConsumerState<LoginScreen> createState() => _LoginScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final TextEditingController emailController = useTextEditingController(
+      text: kDebugMode ? 'eve.holt@reqres.in' : '',
+    );
 
-class _LoginScreenState extends ConsumerState<LoginScreen> {
-  final _emailController = TextEditingController(text: 'eve.holt@reqres.in'); // Test data
-  final _passwordController = TextEditingController(text: 'cityslicka');      // Test data
+    final TextEditingController passwordController = useTextEditingController(
+      text: kDebugMode ? 'cityslicka' : '',
+    );
 
-  final _formKey = GlobalKey<FormState>();
-  bool _isPasswordVisible = false;
+    final ValueNotifier<bool> isPasswordVisible = useState(false);
 
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
+    final GlobalKey<FormState> formKey = useMemoized(
+      () => GlobalKey<FormState>(),
+    );
 
-  // Hàm xử lý Đăng nhập
-  void _onLogin() {
-    if (_formKey.currentState!.validate()) {
-      // Ẩn bàn phím
-      FocusScope.of(context).unfocus();
-
-      // Gọi hàm login trong Notifier
-      ref.read(authControllerProvider.notifier).login(
-        _emailController.text.trim(),
-        _passwordController.text.trim(),
-      );
+    void onLogin() {
+      if (formKey.currentState!.validate()) {
+        FocusScope.of(context).unfocus();
+        ref
+            .read(authControllerProvider.notifier)
+            .login(emailController.text.trim(), passwordController.text.trim());
+      }
     }
-  }
 
-  // Hàm xử lý Đăng ký
-  void _onRegister() {
-    if (_formKey.currentState!.validate()) {
-      FocusScope.of(context).unfocus();
-
-      // Gọi hàm register trong Notifier
-      ref.read(authControllerProvider.notifier).register(
-        _emailController.text.trim(),
-        _passwordController.text.trim(),
-      );
+    void onRegister() {
+      if (formKey.currentState!.validate()) {
+        FocusScope.of(context).unfocus();
+        ref
+            .read(authControllerProvider.notifier)
+            .register(
+              emailController.text.trim(),
+              passwordController.text.trim(),
+            );
+      }
     }
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // 1. Lắng nghe State để xử lý Lỗi (Side Effects)
-    // Lưu ý: Không cần xử lý chuyển trang ở đây vì GoRouter Redirect đã lo việc đó
     ref.listen<AsyncValue<bool>>(authControllerProvider, (previous, next) {
       if (next.hasError && !next.isLoading) {
-        // Hiện thông báo lỗi
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text(
-              next.error.toString().replaceAll('Exception: ', ''), // Bỏ chữ Exception cho đẹp
+              next.error.toString().replaceAll('Exception: ', ''),
               style: const TextStyle(color: Colors.white),
             ),
             backgroundColor: Colors.red,
@@ -75,7 +66,6 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       }
     });
 
-    // 2. Watch State để cập nhật UI (Loading)
     final authState = ref.watch(authControllerProvider);
     final isLoading = authState.isLoading;
 
@@ -85,12 +75,11 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(24.0),
           child: Form(
-            key: _formKey,
+            key: formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                // --- LOGO ---
                 const Icon(
                   Icons.movie_filter_rounded,
                   size: 80,
@@ -106,81 +95,59 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                     color: Colors.indigo,
                   ),
                 ),
-                const SizedBox(height: 8),
-                Text(
-                  'Đăng nhập để khám phá kho phim',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
 
                 const SizedBox(height: 40),
 
-                // --- EMAIL INPUT ---
-                TextFormField(
-                  controller: _emailController,
+                CustomTextField(
+                  controller: emailController,
+                  label: 'Email',
+                  hintText: 'example@gmail.com',
+                  prefixIcon: Icons.email_outlined,
                   keyboardType: TextInputType.emailAddress,
                   textInputAction: TextInputAction.next,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'eve.holt@reqres.in',
-                    prefixIcon: Icon(Icons.email_outlined),
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-                  ),
-                  enabled: !isLoading, // Disable khi đang loading
+                  enabled: !isLoading,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Vui lòng nhập Email';
                     }
-                    if (!value.contains('@')) {
-                      return 'Email không hợp lệ';
-                    }
+                    if (!value.contains('@')) return 'Email không hợp lệ';
                     return null;
                   },
                 ),
                 const SizedBox(height: 16),
 
-                // --- PASSWORD INPUT ---
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: !_isPasswordVisible, // Ẩn/Hiện pass
+                CustomTextField(
+                  controller: passwordController,
+                  label: 'Mật khẩu',
+                  prefixIcon: Icons.lock_outline,
                   textInputAction: TextInputAction.done,
-                  decoration: InputDecoration(
-                    labelText: 'Mật khẩu',
-                    prefixIcon: const Icon(Icons.lock_outline),
-                    border: const OutlineInputBorder(),
-                    contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-                    // Nút con mắt
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isPasswordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _isPasswordVisible = !_isPasswordVisible;
-                        });
-                      },
-                    ),
-                  ),
                   enabled: !isLoading,
+
+                  obscureText: !isPasswordVisible.value,
+
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isPasswordVisible.value
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      isPasswordVisible.value = !isPasswordVisible.value;
+                    },
+                  ),
+
                   validator: (value) {
-                    if (value == null || value.isEmpty) {
+                    if (value == null || value.isEmpty)
                       return 'Vui lòng nhập mật khẩu';
-                    }
-                    if (value.length < 6) {
-                      return 'Mật khẩu phải hơn 6 ký tự';
-                    }
+                    if (value.length < 6) return 'Mật khẩu phải hơn 6 ký tự';
                     return null;
                   },
-                  onFieldSubmitted: (_) => _onLogin(), // Enter là login luôn
+                  onFieldSubmitted: (_) => onLogin(),
                 ),
                 const SizedBox(height: 24),
 
-                // --- LOGIN BUTTON ---
                 FilledButton(
-                  onPressed: isLoading ? null : _onLogin,
+                  onPressed: isLoading ? null : onLogin,
                   style: FilledButton.styleFrom(
                     padding: const EdgeInsets.symmetric(vertical: 16),
                     backgroundColor: Colors.indigo,
@@ -190,56 +157,20 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                   ),
                   child: isLoading
                       ? const SizedBox(
-                    height: 24,
-                    width: 24,
-                    child: CircularProgressIndicator(
-                      color: Colors.white,
-                      strokeWidth: 2,
-                    ),
-                  )
+                          height: 24,
+                          width: 24,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                            strokeWidth: 2,
+                          ),
+                        )
                       : const Text(
-                    'ĐĂNG NHẬP',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-
-                // --- DIVIDER ---
-                const Row(
-                  children: [
-                    Expanded(child: Divider()),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Text(
-                        "HOẶC",
-                        style: TextStyle(color: Colors.grey, fontSize: 12),
-                      ),
-                    ),
-                    Expanded(child: Divider()),
-                  ],
-                ),
-                const SizedBox(height: 24),
-
-                // --- REGISTER BUTTON ---
-                OutlinedButton(
-                  onPressed: isLoading ? null : _onRegister,
-                  style: OutlinedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    side: const BorderSide(color: Colors.indigo),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  child: const Text(
-                    'ĐĂNG KÝ TÀI KHOẢN',
-                    style: TextStyle(
-                      color: Colors.indigo,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                          'ĐĂNG NHẬP',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                 ),
               ],
             ),
