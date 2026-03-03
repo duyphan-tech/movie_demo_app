ba3c5ec310a69d07aefc65924a8ef9c70752be14
Phan Quang Duy <duy.phan@executionlab.asia>
2026-03-03 09:59:11 +0700

    refactor(flavor): use VARIABLE_ONE for deep link scheme in flavorizr config

--- lib/main.dart
+++ lib/main.dart
@@ -2,59 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:movie_demo_app/core/logger/app_logger.dart';
-import 'package:movie_demo_app/core/providers/deep_link_provider.dart';
-import 'package:movie_demo_app/providers/deep_link_stream_provider.dart';
-import 'package:movie_demo_app/core/providers/pending_deep_link_provider.dart';
-import 'package:movie_demo_app/core/providers/storage_providers.dart';
 
-import 'core/configs/app_config.dart';
+import 'app.dart';
 import 'flavors.dart';
-import 'my_app.dart';
 
 void main() {
   F.appFlavor = Flavor.values.firstWhere(
     (element) => element.name == appFlavor,
-    orElse: () => Flavor.dev,
   );
 
-  final config = AppConfig(
-    flavor: F.appFlavor,
-    appName: F.title,
-  );
-
-  mainCommon(config);
-}
-
-Future<void> mainCommon(AppConfig config) async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final container = ProviderContainer(
-    overrides: [appConfigProvider.overrideWithValue(config)],
-  );
-
-  try {
-    await container.read(appInitializationProvider.future);
-
-    final pendingDeepLinkNotifier = container.read(
-      pendingDeepLinkProvider.notifier,
-    );
-    await pendingDeepLinkNotifier.initializeAndCheckDeepLink();
-
-    container.read(deepLinkStreamProvider);
-
-    container.read(appsFlyerInitializerProvider.future).catchError((e) {
-      AppLogger.e('AppsFlyer Init Error', tag: 'Main', error: e);
-    });
-  } catch (e) {
-    AppLogger.e('Initialization error', tag: 'Main', error: e);
-  }
-
-  runApp(
-    UncontrolledProviderScope(
-      container: container,
-      child: MyApp(config: config),
-    ),
-  );
+  runApp(const App());
 }
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movie_demo_app/flavors.dart';
+import '../flavors.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
